b99264a6f3f78e88b475b718b44524226f9a083a
Aymeric LAVIT-D-HAUTEFORT <alavit-d@e1r4p11.42.fr>
2015-04-02 10:19:31 +0200

    reroll water version

--- water.js
+++ water.js
@@ -1,17 +1,8 @@
-var water = new Float64Array(32 * 32);
-var ground = new Float64Array(32 * 32);
-var width = 32;
+var water = new Float64Array(1024 * 1024);
+var width = 1024;
 
 for (var i = 0; i < water.length; i++)
-	water[i] = 50;
-
-function generateHill(data, width, x1, y1) {
-	for (var i = 0; i < width * width; i++) {
-		var x = i % width, y =  Math.floor(i / width);
-		tmp = Math.pow(10, 2) - (Math.pow(x - x1, 2) + Math.pow(y - y1, 2));
-		data[i] += (tmp > 0 ? Math.floor(tmp) : 0);
-	}
-}
+	water[i] = 60;
 
 function	height(i) {
 	if (0 <= i && i < ground.length)
@@ -33,7 +24,6 @@ function	nbrFlow(i) {
 function	moveWater(a, b, n) {
 	if (height(a) > height(b)) {
 		var vol = (height(a) - height(b)) / (n * 2);
-		//console.log(a + ' to ' + b + ' vol: ' + vol);
 		water[a] -= vol;
 		water[b] += vol;
 	}
@@ -50,22 +40,3 @@ function	fluid(ground, water) {
 		}
 	}
 }
-
-function print() {
-	for (var i = 0; i < 32; i++) {
-		var str = '';
-		for (var j = 0; j < 32; j++) {
-			str += ' ' + (height(j + i * 32));
-		}
-		console.log(str);
-	}
-}
-console.log('printing');
-print();
-generateHill(ground, 32, 16, 16);
-console.log('printing');
-print();
-for (var i = 0; i < 10; i++)
-	fluid(water, ground);
-console.log('printing');
-print();
